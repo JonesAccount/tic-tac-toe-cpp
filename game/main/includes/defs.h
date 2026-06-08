7738c6723f10ec8c39595baf2833713a3270df52
jons <nooneprofessor@gmail.com>
2026-06-08 12:43:12 +0500

    c

---
+++ game/main/includes/defs.h
@@ -0,0 +1,12 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define YELLOW "\033[33m"
+#define GRAY "\033[90m"
+#define RED "\033[31m"
+#define N "\n"
+#define TAB "\t"
+
+#endif
---
+++ game/main/includes/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+char getch(void);
+void clear_screen(void);
+
+#endif
